1a4a3c13d92b6f6aad1ec466117bdba7e5ee5caa
Callum <cal.woods@hotmail.co.uk>
2022-12-13 20:57:07 +0000

    + optimal stopping threshold func and tests

--- optimal_petting/utils.py
+++ optimal_petting/utils.py
@@ -1,6 +1,7 @@
 import random 
 import numpy as np
-
+from scipy.stats import rv_continuous
+from typing import List, Tuple
 
 def random_attractiveness(option):
     return random.uniform(0, 1)
@@ -28,3 +29,61 @@ def get_relative_rank(x):
         rr[i] = n_values_better + 1  
 
     return rr
+
+
+def weight_expected_value(expected_value, dist):
+    return expected_value * dist.cdf(expected_value)
+
+
+def get_optimal_thresholds(
+    n_options: int, 
+    dist: rv_continuous
+    ) -> Tuple[List[float], List[float]]:
+    """ 
+    Calculates the optimal stopping thresholds via backward
+    induction, replicating the methodology used in:
+
+      A linear threshold model for optimal stopping behavior
+        https://doi.org/10.1073/pnas.2002312117
+
+    - The threshold of the final item is ∞, because the
+      rules of the task stipulate that the final item must be
+      accepted if no earlier item has been chosen.
+       
+    - The thresholds for the previous items are determined by
+      working backward from the final item, using conditional
+      expectations.
+
+    Important Assumptions:
+    - Lower values are better/more rewarding (e.g., Price)
+
+    Params:
+        n_options (Int): number of observations to choose from 
+                        (e.g., candidates).
+        dist (scipy.stats.rv_continuous): the distribution the
+                        options were sampled from.
+    Returns: thresholds, expected_values
+    """
+    # thresh = the acceptance threshold, expected = expected value
+    thresh, expected = np.zeros(n_options), np.zeros(n_options)
+
+    # Treat the start of the list as the last option seen (reverse list at end).
+    thresh[0] = np.inf
+    expected[0] = dist.mean()
+    thresh[1] = expected[0]
+
+    for i in range(1, n_options-1):
+        # expected value of the area under the probabilty curve up until the threshold
+        expected[i] = dist.expect(lb=-np.inf, ub=thresh[i])
+        
+        # probability of the expected value 
+        prob = dist.cdf(thresh[i]) 
+        
+        thresh[i+1] = (expected[i] * prob) + ( # expected value of item * it's probability
+                expected[i-1] * (1-prob) # the expected value of previous item * its probability
+                )        
+
+    # Fill in the remaining expectation value
+    expected[-1] = dist.expect(lb=-np.inf, ub=thresh[-1])
+
+    return thresh[::-1], expected[::-1]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,8 +2,10 @@ import pytest
 from optimal_petting.utils import (
     get_relative_rank,
     random_attractiveness,
-    get_sample_from_norm
+    get_sample_from_norm,
+    get_optimal_thresholds
 )
+from scipy.stats.distributions import norm 
 
 @pytest.mark.parametrize(
     argnames=('x', 'expected'), 
@@ -33,3 +35,32 @@ def test_random_attractiveness():
 def test_get_random_norm():
     print(get_sample_from_norm(10))
     pass
+
+
+@pytest.mark.parametrize(
+    argnames=('sequence', 'prior'),
+    argvalues=(
+        ([0.9,0.9,0.1,0.9, 0.9], (0.5, 0.3)),
+        ([0.8,0.9, 0.75, 0.82, 0.94], (1, 0.3)),
+    )
+)
+def test_get_optimal_thresholds(sequence, prior):
+
+    thresholds, expected_values = get_optimal_thresholds(
+        n_options=len(sequence),
+        dist=norm(prior[0], prior[1]),
+    )
+
+    # strictly increasing thresholds 
+    assert all(i < j for i, j in zip(thresholds, thresholds[1:]))
+
+    # all positive (although negative values could be valid!)
+    assert all(i > 0 for i in thresholds)
+
+    # the last option is always chosen
+    assert thresholds[-1] > sequence[-1]
+
+    # print(f'seq: {sequence}, \n')
+    # print(f'thresholds: {thresholds}, \n')
+    # print(f'expected values: {expected_values} \n')
+    # print(sequence < thresholds)
